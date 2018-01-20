06aa9f426a127c37666fcf4ecffd9d0902540a58
junhobaik <junhobaik@gmail.com>
2018-01-20 21:54:55 +0900

    폴더 구조

--- 1-vanilla/js/views/View.js
+++ 1-vanilla/js/views/View.js
@@ -1,22 +1,26 @@
 const tag = '[View]'
 
-export default {
+export default { 
+  // ES6 module 패턴으로 아래 init on... 객체를 export
   init(el) {
     if (!el) throw el
     this.el = el
+    // 받은 element를 자신의 프로퍼티로 갖게 된다.
     return this
   },
+
 
   on(event, handler) {
     this.el.addEventListener(event, handler)
+    // 이벤트와 헨들러를 연결지어준다
     return this
   },
-
   emit(event, data) {
     const evt = new CustomEvent(event, { detail: data })
     this.el.dispatchEvent(evt)
     return this
   },
+
 
   hide() {
     this.el.style.display = 'none'
