92301a0a83aef99061798b33efe229aa04607045
junhobaik <junhobaik@gmail.com>
2018-01-20 22:19:34 +0900

    검색폼 구현3

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -5,6 +5,12 @@ const tag = '[MainController]';
 export default {
     init(){
         console.log(tag, 'init()');
-        FormView.setup(document.querySelector('form'));
+        FormView.setup(document.querySelector('form'))
+            //on을 사용하려면 FormView.setup에서 this가 return되는지 확인해야한다.
+            .on('@submit', e => this.onSubmit(e.detail.input))
+    },
+
+    onSubmit(input){
+        console.log(tag, 'onSubmit()', input);
     }
 }
