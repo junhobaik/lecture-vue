7e2a23b261d126f5a1416552b7f31ec3cdbbc91c
junhobaik <junhobaik@gmail.com>
2018-01-23 23:41:21 +0900

    검색결과 구현1

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,4 +1,7 @@
 import FormView from '../views/FormView.js';
+import ResultView from '../views/ResultView.js';
+
+import SearchModel from '../models/SearchModel.js';
 
 const tag = '[MainController]';
 
@@ -9,13 +12,30 @@ export default {
             //on을 사용하려면 FormView.setup에서 this가 return되는지 확인해야한다.
             .on('@submit', e => this.onSubmit(e.detail.input))
             .on('@reset', e => this.onResetForm())
+
+        ResultView.setup(document.querySelector('#search-result'))
+            
+    },
+
+    search(query){
+        console.log(tag, 'search()', query);
+        SearchModel.list(query)
+            .then(data => {
+                this.onSearchResult(data);
+            })
+        this.onSearchResult([]);
     },
 
     onSubmit(input){
         console.log(tag, 'onSubmit()', input);
+        this.search(input);
     },
 
     onResetForm(){
         console.log(tag, 'onReset()');
+    },
+
+    onSearchResult(data){
+        ResultView.render(data);
     }
 }
