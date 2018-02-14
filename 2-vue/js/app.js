3295ca10864ea3010236d9e2fc1484fe2f19b3c9
junhobaik <junhobaik@gmail.com>
2018-02-14 17:48:14 +0900

    탭

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -5,8 +5,14 @@ new Vue({
   data: {
     query: '',
     submitted: false,
+    tabs: ['추천 검색어', '최근 검색어'],
     searchResult: []
   },
+
+  created(){
+    this.selectedTab = this.tabs[0];
+  },
+
   methods: {
     onSubmit(e){
       this.search();
