bdca1369e34b5068908d7491388e66a21f2759aa
junhobaik <junhobaik@gmail.com>
2018-02-14 17:19:55 +0900

    검색폼 (구현)

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -6,6 +6,16 @@ new Vue({
   methods: {
     onSubmit(e){
       debugger;
+    },
+    onKeyup(){
+      if(!this.query.length){
+        this.onReset();
+      }
+    },
+    onReset(){
+      this.query = '';
+      // TODO: 검색결과 숨기기
+      debugger;
     }
   }
 })
