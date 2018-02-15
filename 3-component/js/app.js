b365774f195c8669037e94359f6d3986e69bcc8a
junhobaik <junhobaik@gmail.com>
2018-02-15 17:36:28 +0900

    FormComponent 구현 1

--- 3-component/js/app.js
+++ 3-component/js/app.js
@@ -2,6 +2,9 @@ import SearchModel from './models/SearchModel.js'
 import KeywordModel from './models/KeywordModel.js'
 import HistoryModel from './models/HistoryModel.js'
 
+import FormComponent from './components/FormComponent.js';
+
+
 new Vue({
   el: '#app',
   data: {
@@ -13,11 +16,17 @@ new Vue({
     history: [],
     searchResult: []
   },
+
+  components: {
+    'search-form': FormComponent
+  },
+  
   created() {
     this.selectedTab = this.tabs[0]
     this.fetchKeyword()
     this.fetchHistory()
   },
+
   methods: {
     onSubmit(e) {
       this.search()
