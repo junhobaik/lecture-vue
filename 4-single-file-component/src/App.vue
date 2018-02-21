dca1d0e326f905af62f794db1df6bbb18d37ee38
junhobaik <junhobaik@gmail.com>
2018-02-21 23:26:35 +0900

    단일 컴포넌트 3

--- 4-single-file-component/src/App.vue
+++ 4-single-file-component/src/App.vue
@@ -3,16 +3,76 @@
     <header>
       <h2 class="container">검색</h2>
     </header>
+
+    <div class="container">
+      <search-form v-bind:value="query" v-on:@submit="onSubmit" v-on:@reset="onReset">
+      </search-form>
+    </div>
   </div>
 </template>
 
 <script>
+  import FormComponent from '../src/components/FormComponent.vue';
+
   export default {
     name: 'app',
     data() {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        query: '',
+        submitted: false,
+        tabs: ['추천 검색어', '최근 검색어'],
+        selectedTab: '',
+        keywords: [],
+        history: [],
+        searchResult: []
+      }
+    },
+    components: {
+      'search-form': FormComponent
+    },
+    methods: {
+      onSubmit(inputValue) {
+        this.query = inputValue;
+        this.search()
+      },
+      onReset(e) {
+        this.resetForm()
+      },
+      onClickTab(tab) {
+        this.selectedTab = tab
+      },
+      onClickKeyword(keyword) {
+        this.query = keyword;
+        this.search()
+      },
+      onClickRemoveHistory(keyword) {
+        HistoryModel.remove(keyword)
+        this.fetchHistory()
+      },
+      fetchKeyword() {
+        KeywordModel.list().then(data => {
+          this.keywords = data
+        })
+      },
+      fetchHistory() {
+        HistoryModel.list().then(data => {
+          this.history = data
+        })
+      },
+      search() {
+        SearchModel.list().then(data => {
+          this.submitted = true
+          this.searchResult = data
+        })
+        HistoryModel.add(this.query)
+        this.fetchHistory()
+      },
+      resetForm() {
+        this.query = ''
+        this.submitted = false
+        this.searchResult = []
       }
     }
   }
+
 </script>
